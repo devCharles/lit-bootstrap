292ea7a755f024ce93ffd869953229f66dcb9b32
DevCharles <charlesipn@gmail.com>
2023-03-15 12:55:03 -0600

    init

---
+++ src/my-element.js
@@ -0,0 +1,67 @@
+import { LitElement, css, html } from "lit";
+import litLogo from "./assets/lit.svg";
+
+/**
+ * An example element.
+ *
+ * @slot - This element has a slot
+ * @csspart button - The button
+ */
+export class MyElement extends LitElement {
+  static get properties() {
+    return {
+      /**
+       * Copy for the read the docs hint.
+       */
+      docsHint: { type: String },
+
+      /**
+       * The number of times the button has been clicked.
+       */
+      count: { type: Number },
+    };
+  }
+
+  // createRenderRoot() {
+  //   return this;
+  // }
+
+  constructor() {
+    super();
+    this.docsHint = "Click on the Vite and Lit logos to learn more";
+    this.count = 0;
+  }
+
+  render() {
+    return html`
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" class="logo" alt="Vite logo" />
+        </a>
+        <a href="https://lit.dev" target="_blank">
+          <img src=${litLogo} class="logo lit" alt="Lit logo" />
+        </a>
+      </div>
+      <slot></slot>
+      <div class="card">
+        <button @click=${this._onClick} part="button">
+          count is ${this.count}
+        </button>
+      </div>
+      <p class="read-the-docs">${this.docsHint}</p>
+      <div class="alert alert-primary" role="alert">
+        A simple primary alert—check it out!
+      </div>
+    `;
+  }
+
+  _onClick() {
+    this.count++;
+  }
+
+  static get styles() {
+    return css``;
+  }
+}
+
+window.customElements.define("my-element", MyElement);
